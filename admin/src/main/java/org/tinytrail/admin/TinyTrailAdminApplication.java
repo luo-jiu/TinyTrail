6e1ddd19c9e98b7e1bc914cfd296099a2c249d54
luo-jiu <z3201132097@gmail.com>
2024-06-24 10:37:52 +0800

    项目整体构建

---
+++ admin/src/main/java/org/tinytrail/admin/TinyTrailAdminApplication.java
@@ -0,0 +1,12 @@
+package org.tinytrail.admin;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyTrailAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TinyTrailAdminApplication.class, args);
+    }
+}
